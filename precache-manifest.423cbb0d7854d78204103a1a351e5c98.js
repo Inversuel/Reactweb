baa400b548dd7d59f2d81cd3a6739a10ffe3f3b2
Maj Karol <k.maj@builditup.eu>
2020-08-26 14:25:59 +0200

    Updates

---
+++ precache-manifest.423cbb0d7854d78204103a1a351e5c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ac4acba9c380103209360e4525e422",
+    "url": "/Reactweb/index.html"
+  },
+  {
+    "revision": "ee26c0f0d8bf28c994ae",
+    "url": "/Reactweb/static/css/main.da56effb.chunk.css"
+  },
+  {
+    "revision": "ee9c2ac9c920b576a833",
+    "url": "/Reactweb/static/js/2.3fb50242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactweb/static/js/2.3fb50242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee26c0f0d8bf28c994ae",
+    "url": "/Reactweb/static/js/main.fef9406b.chunk.js"
+  },
+  {
+    "revision": "d41597f1e7ea3856fc36",
+    "url": "/Reactweb/static/js/runtime-main.21b1f0b0.js"
+  }
+]);
